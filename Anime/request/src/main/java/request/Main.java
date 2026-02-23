e700b58fc6f8e85600033d43b0c2059cb73ae5e4
Final-Eclipse <AnimeAndManga1@proton.me>
2026-02-23 00:58:14 -0500

    Add files via upload

---
+++ Anime/request/src/main/java/request/Main.java
@@ -0,0 +1,16 @@
+package request;
+
+import java.io.IOException;
+// import java.net.URI;
+// import java.net.http.HttpClient;
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
+// import java.net.http.HttpResponse.BodyHandlers;
+
+public class Main 
+{
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
+        Request.getRequest();
+    }    
+}

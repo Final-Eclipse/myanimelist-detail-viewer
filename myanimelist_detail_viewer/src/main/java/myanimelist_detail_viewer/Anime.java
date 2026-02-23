b45ad0c86f7210576dcae97905eae9ff791e9864
Final-Eclipse <AnimeAndManga1@proton.me>
2026-02-23 01:25:35 -0500

    Add files via upload

---
+++ myanimelist_detail_viewer/src/main/java/myanimelist_detail_viewer/Anime.java
@@ -0,0 +1,22 @@
+package myanimelist_detail_viewer;
+
+public class Anime 
+{
+    String name;
+    String animeType; 
+    int episodes;
+    boolean isAiring;
+    String airingStartDate;
+    String airingEndDate; 
+    String seasonPremiered;
+    String broadcastTime;
+    String[] producers;
+    String[] licensors;  
+    String[] studios;
+    String sourceMaterial;
+    String[] genres;
+    String[] themes;
+    int durationPerEpisode;
+    String rating;
+    int users;
+}
---
+++ myanimelist_detail_viewer/src/main/java/myanimelist_detail_viewer/Isekai.java
@@ -0,0 +1,5 @@
+package myanimelist_detail_viewer;
+
+public class Isekai {
+    
+}
